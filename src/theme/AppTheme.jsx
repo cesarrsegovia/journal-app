b098e447b2d3a305fb8f94de027a00dbad39939c
Cesar Segovia <cesarr.segovia27@gmail.com>
2024-08-14 22:42:28 -0300

    cambios en apptheme

--- src/theme/AppTheme.jsx
+++ src/theme/AppTheme.jsx
@@ -5,6 +5,7 @@ import { purpleTheme } from "./"
 
 export const AppTheme = ({ children }) => {
     return (
+        //themeprovider con props children para agregar la libreria mui material
         <ThemeProvider theme={ purpleTheme }>
             <CssBaseline/>
 
