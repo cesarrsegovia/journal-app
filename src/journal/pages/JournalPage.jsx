c26b8b1cc388690293338abbe3c23dd54ec58afc
Cesar Segovia <cesarr.segovia27@gmail.com>
2024-08-14 22:41:28 -0300

    cambio en journalpage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -4,6 +4,7 @@ import { Typography } from "@mui/material"
 export const JournalPage = () => {
     return (
         <>
+            //agregando componente de tipografia para exportar al journal page
             <Typography variant='h1'>JournalPage</Typography>
         </>
     )
